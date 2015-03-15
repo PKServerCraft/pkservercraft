c35a4c544a9b7b1be5f2e7c9fc98b3a462bac832
Paul Kimbrel <paul@paulkimbrel.com>
2015-03-15 12:34:34 -0500

    Added authentication support

--- start.js
+++ start.js
@@ -2,6 +2,7 @@
 /*jslint plusplus: true */
 
 var Express = require('express');
+var stormpath = require('express-stormpath');
 var cors = require('cors');
 var serverManager = require('./lib/ServerManager');
 
@@ -32,7 +33,37 @@ function main() {
         response.set('Content-Type', 'application/json').status(500).send(retVal);
     }
 
-    app.get(CONTEXT_ROOT + "/servers", function (request, response) {
+    app.use(stormpath.init(app, {
+        application: "https://api.stormpath.com/v1/applications/6nmDEYUaVCWYNuxHivKPzm",
+        enableAccountVerification: true,
+        enableForgotPassword: true,
+        registrationUrl: CONTEXT_ROOT + '/register',
+        loginUrl: CONTEXT_ROOT + '/login',
+        logoutUrl: CONTEXT_ROOT + '/logout',
+        postLogoutRedirectUrl: CONTEXT_ROOT + '/servers',
+        resendAccountVerificationEmailUrl: CONTEXT_ROOT + '/verification/resend',
+        forgotPasswordUrl: CONTEXT_ROOT + '/forgot',
+        postForgotPasswordRedirectUrl: CONTEXT_ROOT + '/forgot/sent',
+        forgotPasswordChangeUrl: CONTEXT_ROOT + '/forgot/change',
+        postForgotPasswordChangeRedirectUrl: CONTEXT_ROOT + '/forgot/change/done',
+        accountVerificationCompleteUrl: CONTEXT_ROOT + '/verified',
+        getOauthTokenUrl: CONTEXT_ROOT + '/oauth',
+        redirectUrl: CONTEXT_ROOT + '/me',
+        facebookLoginUrl: CONTEXT_ROOT + '/facebook',
+        googleLoginUrl: CONTEXT_ROOT + '/google',
+        idSiteUrl: CONTEXT_ROOT + '/redirect',
+        idSiteRegistrationUrl: CONTEXT_ROOT + '/#register'
+    }));
+
+    app.get(CONTEXT_ROOT + '/me', stormpath.authenticationRequired, function (req, res) {
+        var user = req.user;
+        res.json({
+            name: user.givenName + " " + user.surname,
+            email: user.email
+        });
+    });
+
+    app.get(CONTEXT_ROOT + "/servers", stormpath.authenticationRequired, function (request, response) {
         serverManager.listServers().then(function (servers) {
             sendResponse(response, "servers", "success", servers);
         }, function (error) {
